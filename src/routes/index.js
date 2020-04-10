5114653556bbdb61fb9e6e4f463c957d05efaeb1
luizpaulogroup <bylpgaming@gmail.com>
2020-04-10 20:03:25 -0300

    Add configuration tabs of createBottomTabNavigator

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
 
 import Home from '../pages/Home';
 import Account from '../pages/Account';
@@ -29,10 +31,54 @@ const Tabs = createBottomTabNavigator();
 
 const Routes = () => (
     <NavigationContainer>
-        <Tabs.Navigator>
-            <Tabs.Screen name="Home" component={myStack} />
-            <Tabs.Screen name="Account" component={Account} />
-            <Tabs.Screen name="Configuration" component={Configuration} />
+        <Tabs.Navigator tabBarOptions={{
+            tabStyle: {
+                backgroundColor: '#FFF',
+            },
+            activeTintColor: '#000',
+        }}>
+            <Tabs.Screen
+                name="Home"
+                component={myStack}
+                options={{
+                    title: "HOME",
+                    tabBarLabel: ({ color }) => (
+                        <View>
+                            <Text style={{ fontWeight: 'bold', color, fontSize: 10 }}>HOME</Text>
+                        </View>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size - 5} />
+                    ),
+                }} />
+            <Tabs.Screen
+                name="Account"
+                component={Account}
+                options={{
+                    title: "ACCOUNT",
+                    tabBarLabel: ({ color }) => (
+                        <View>
+                            <Text style={{ fontWeight: 'bold', color, fontSize: 10 }}>ACCOUNT</Text>
+                        </View>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={size - 5} />
+                    ),
+                }} />
+            <Tabs.Screen
+                name="Configuration"
+                component={Configuration}
+                options={{
+                    title: "CONFIGURATION",
+                    tabBarLabel: ({ color }) => (
+                        <View>
+                            <Text style={{ fontWeight: 'bold', color, fontSize: 10 }}>CONFIG</Text>
+                        </View>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="wrench" color={color} size={size - 5} />
+                    ),
+                }} />
         </Tabs.Navigator>
     </NavigationContainer>
 );
